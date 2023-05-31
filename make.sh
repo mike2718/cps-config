7b510164b5cf2fae1a079122e7c63f38892303e2
Mike Akiba <mike2718@gmail.com>
2023-05-31 19:06:27 +0800

    添加UV-83配置

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git add *.md *.sh *.dat *.csv *.cxf *.cps
+git add *.md *.sh *.dat *.csv *.cxf *.cps *.uvdat
 #git archive --format=zip --prefix=cps-config/ --output=../cps-config.zip main
