e745059735a05bec01e9cff4e0ddfc0be3df0c61
Mike Akiba <mike2718@gmail.com>
2023-06-24 17:41:41 +0800

    更新readme，添加配置文件

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git add *.md *.sh *.dat *.csv *.cxf *.cps *.uvdat *.m19
+git add *.md *.sh *.dat *.csv *.cxf *.cps *.uvdat *.m19 .gitattributes
 git archive --format=zip --prefix=cps-config/ --output=/d/cps-config.zip main
