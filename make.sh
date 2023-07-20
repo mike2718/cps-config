f5e3dd5ac429e4bdd6aee3861e199a61ec6a6bfe
Mike Akiba <mike2718@gmail.com>
2023-07-21 02:08:37 +0800

    备份UV-5R镜像文件

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git add *.md *.sh *.dat *.csv *.cxf *.cps *.uvdat *.m19 .gitattributes images/*.png
+git add *.md *.sh *.dat *.img *.csv *.cxf *.cps *.uvdat *.m19 .gitattributes
 git archive --format=zip --prefix=cps-config/ --output=/d/cps-config.zip main
