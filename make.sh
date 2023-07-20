f2a38d0f4e46364ff73eedfe74ecb30b7378f983
Mike Akiba <mike2718@gmail.com>
2023-07-21 03:03:30 +0800

    更新make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git add *.md *.sh *.dat *.img *.csv *.cxf *.cps *.uvdat *.m19 .gitattributes
-git archive --format=zip --prefix=cps-config/ --output=/d/cps-config.zip main
+git archive --format=tar.zst --prefix=cps-config/ --output=/d/cps-config.tar.zst main
