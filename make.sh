3d1f823b43253f4ab348c8263a8cf18e0bea236a
Saori Isikawa <saori.isikawa414@gmail.com>
2023-10-07 04:52:37 +0800

    更新make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #git add *.md make.sh .gitattributes
 git archive --format=tar.xz --prefix=cps-config/ --output=/d/cps-config.tar.xz main
+git archive --format=tar.xz --prefix=cps-config/ --output=../cps-config.tar.xz main
