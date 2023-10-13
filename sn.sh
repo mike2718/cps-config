9feaa0cbb3c9deb66bbf071c697f5c7a99d1dbe6
Saori Isikawa <saori.isikawa414@gmail.com>
2023-10-13 15:44:16 +0800

    修订sn生成shell脚本

--- sn.sh
+++ sn.sh
@@ -4,5 +4,7 @@ tree -v -P '*.img|*.dat|*.uvdat|*.m19' --noreport | sed 's/\.[^.]*$//'
 rm sn.md
 echo '# 设备序列号列表' > sn.md
 echo >> sn.md
+echo '```' >> sn.md
 tree -v  -P '*.img|*.dat|*.uvdat|*.m19' --noreport >> sn.md
+echo '```' >> sn.md
 echo >> sn.md
