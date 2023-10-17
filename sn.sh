9f60ed8504a3d76416c48e75f89b9dff30b1391f
Saori Isikawa <saori.isikawa414@gmail.com>
2023-10-17 14:57:36 +0800

    更新sn.sh

--- sn.sh
+++ sn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 tree -v -P '*.img|*.dat|*.uvdat|*.m19' --noreport | sed 's/\.[^.]*$//'
 
-rm sn.md
-echo '# 设备序列号列表' > sn.md
-echo >> sn.md
-echo '```' >> sn.md
-tree -v  -P '*.img|*.dat|*.uvdat|*.m19' --noreport >> sn.md
-echo '```' >> sn.md
-echo >> sn.md
+#rm sn.md
+#echo '# 设备序列号列表' > sn.md
+#echo >> sn.md
+#echo '```' >> sn.md
+#tree -v  -P '*.img|*.dat|*.uvdat|*.m19' --noreport >> sn.md
+#echo '```' >> sn.md
+#echo >> sn.md
