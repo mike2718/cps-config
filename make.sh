35c0c6de286ae7c794438424b4044cb7f4e6bacb
Mike Akiba <mike2718@gmail.com>
2023-10-18 01:14:14 +0800

    修改make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#git add *.md make.sh .gitattributes
+git pull
+git push
 git archive --format=tar.xz --prefix=cps-config/ --output=/d/cps-config.tar.xz main
 git archive --format=tar.xz --prefix=cps-config/ --output=../cps-config.tar.xz main
