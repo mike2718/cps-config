ee77a33b70c40b180b4ac75728d6b50ca2d73453
Mike Akiba <mike2718@gmail.com>
2023-11-02 01:04:02 +0800

    更新make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 git pull
-git push
-git archive --format=tar.xz --prefix=cps-config/ --output=/d/cps-config.tar.xz main
 git archive --format=tar.xz --prefix=cps-config/ --output=../cps-config.tar.xz main
+cp -f ../cps-config.tar.xz /d/
