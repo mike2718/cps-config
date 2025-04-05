b947bf40103b0ef4a7d839579fa202e6efe33939
Mike <mike2718@gmail.com>
2025-04-05 20:41:22 +0800

    更新make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 git pull
-git archive --format=tar.xz --prefix=cps-config/ --output=../cps-config.tar.xz main
-cp -f ../cps-config.tar.xz /d/
+git archive --format=zip --prefix=cps-config/ --output=/d/cps-config.zip main
